becc1e1eb0732c406f6c0dcf241fe58aa9376425
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-06-13 12:50:03 -0300

    Configuration

--- aGrow/src/main/java/com/talissonmelo/AGrowApplication.java
+++ aGrow/src/main/java/com/talissonmelo/AGrowApplication.java
@@ -2,20 +2,12 @@ package com.talissonmelo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableWebMvc
-public class AGrowApplication implements WebMvcConfigurer{
+public class AGrowApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AGrowApplication.class, args);
-	}
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("GET", "POST", "DELETE", "PUT");
 	}
 }
